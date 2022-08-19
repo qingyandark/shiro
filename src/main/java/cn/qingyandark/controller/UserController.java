fe10016fe364fad6e6a004f8d4fb46a5d9ab994c
qingyandark <qingyandark@163.com>
2022-08-19 20:00:49 +0800

    contorller -> controller

--- src/main/java/cn/qingyandark/controller/UserController.java
+++ src/main/java/cn/qingyandark/controller/UserController.java
@@ -1,10 +1,9 @@
-package cn.qingyandark.contorller;
+package cn.qingyandark.controller;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
